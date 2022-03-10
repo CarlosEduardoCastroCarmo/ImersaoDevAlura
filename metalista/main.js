59bb90e9deb2019b45473da6a55349453983e45b
Carlos Eduardo Castro do Carmo <carloseduardocastrodocarmo@gmail.com>
2022-03-10 20:52:39 -0300

    Projeto terminado

---
+++ metalista/main.js
@@ -0,0 +1,54 @@
+//Indetificando atribuindo o momento do clique do usuário a uma variável;
+const $button = document.querySelector("#enviar");
+const $input = document.querySelector("#takevalue");
+const $resultado = document.querySelector("#resultado");
+const $tentativas = document.querySelector("#quantidadeTentativas");
+
+var totalTentativas = 3;
+var numeroRandomico = parseInt(Math.random()*11);
+
+function Mentalista(event){
+    event.preventDefault();
+
+    qtdTentativas = totalTentativas - 1;
+
+    var valorIput = parseInt($input.value);
+    if(valorIput > 10){
+        $resultado.innerHTML = "Tentativa inválida, número informado é maior que 10!"
+    }
+    else if(numeroRandomico != valorIput && valorIput < numeroRandomico){
+        $resultado.innerHTML = "Voce errou, o número informado é menor que nosso numero secreto"
+    }
+    else if(numeroRandomico != valorIput && valorIput > numeroRandomico){
+        $resultado.innerHTML = "Voce errou, o número informado é maior que nosso numero secreto"
+    }
+    else if(valorIput == numeroRandomico){
+        $resultado.innerHTML = "Uooow, parece que temos um vencedor. Parabéns, você acertou!!"
+    }
+    else{
+        $resultado.innerHTML = "Campo Input está vazio!"
+    }    
+}
+
+function ContarTentativas(event){
+    event.preventDefault();
+
+    totalTentativas -= 1;
+
+    if(totalTentativas < 0){
+        $tentativas.innerHTML = "Voce perdeu, suas tentativas foram esgotadas!"
+        totalTentativas = 3;        
+    }
+    else{
+        $tentativas.innerHTML = "Tentativas restantes: " + totalTentativas;
+    }    
+}
+
+$button.addEventListener("click", Mentalista);
+$button.addEventListener("click", ContarTentativas);
+
+
+
+
+
+
